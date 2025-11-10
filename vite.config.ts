c25f44fc7e0116601a96f4ae73075646988709ca
Harshith Reddy <Harshithreddy0115@gmail.com>
2025-11-10 13:32:54 -0600

    Setup GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/PanInsight/',
+  base: '/frontend-SRMA-Liver/',
   plugins: [react()],
   server: {
     port: 3000,
